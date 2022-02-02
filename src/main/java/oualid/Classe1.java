c1bc9c8022af78cf818bf9a11f3ceb11405052f9
Oualid Sahbani <oualidsahbani@me.com>
2022-02-02 15:13:09 +0100

    mavenfirsttest

---
+++ src/main/java/oualid/Classe1.java
@@ -0,0 +1,9 @@
+package oualid;
+
+public class Classe1 {
+
+        public static boolean checkString(String input) {
+            return input.length() > 20 && input.contains("fancy");
+        }
+
+}
